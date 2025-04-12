934068f73d4596f35ec822649c56d3944ce5de5c
Elias <eliaswu466@gmail.com>
2025-04-11 22:03:14 -0400

    added assets

--- src/main.js
+++ src/main.js
@@ -1,1 +1,38 @@
 import "./style.css";
+import Phaser, { Scene } from "phaser";
+
+const sizes = {
+  width: 800,
+  height: 800,
+};
+
+const speedDown = 300;
+
+class GameScene extends Phaser.Scene {
+  constructor() {
+    super("scene-game");
+  }
+
+  preload() {}
+
+  create() {}
+
+  update() {}
+}
+
+const config = {
+  type: Phaser.WEBGL,
+  width: sizes.width,
+  height: sizes.height,
+  canvas: gameCanvas,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: speedDown },
+      debug: true,
+    },
+  },
+  scene: [GameScene],
+};
+
+const game = new Phaser.Game(config);
